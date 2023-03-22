eb210be86abd2d1836578adf1e276fb0f08569c0
Your Name <you@example.com>
2023-03-22 14:54:00 -0700

    cool

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,3 +1,4 @@
+carrie@ubuntu:/debugging$ cat main.c
 #include "main.h"
 /**
 * main - tests function that prints if integer is positive or negative
@@ -5,8 +6,8 @@
 */
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
+        int i;
+        i = 0;
+        positive_or_negative(i);
+        return (0);
 }
