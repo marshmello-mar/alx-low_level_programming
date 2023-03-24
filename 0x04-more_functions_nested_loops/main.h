a95416354160251d72fcbf202126723a56b8fdc7
Your Name <you@example.com>
2023-03-23 17:46:19 -0700

    cool

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-int _putchar(char c);
-int _isuppert(int c);
+int _putcha(char x);
+int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
